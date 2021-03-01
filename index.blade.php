b4f2efe507f2bb2019ec281ce1eef001b9aa92c5
suriya123-cyber <54349372+suriya123-cyber@users.noreply.github.com>
2021-03-01 12:57:49 +0600

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome home! {{ session('username') }} </h1>
+    <a href="/home/create">Create user</a> |
+    <a href="/home/userlist">View user list</a> |
+    <a href="/logout">logout</a>
+</body>
+</html>
